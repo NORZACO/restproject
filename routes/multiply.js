c6f2f6ae7ff1caf5b703fa9d9b922193fbacad4b
NORZACO <caspermotive4@gmail.com>
2023-04-12 19:49:49 +0200

    functionalities/Error handling

--- routes/multiply.js
+++ routes/multiply.js
@@ -1,29 +1,19 @@
-// // routes/multiply.js:
-// var express = require('express');
-// var router = express.Router();
-
-// router.get('/:number1/:number2', function(req, res, next) {
-//   res.status(200).json(parseInt(req.params.number1) * parseInt(req.params.number2));
-// });
-
-// module.exports = router;
-
 var express = require('express');
+var jsend = require('jsend');
 var router = express.Router();
 
-// Route for multiplying two numbers
-router.get('/:multiplicand/:multiplier', function(req, res, next) {
-  // Parse the values of the parameters and handle errors
-  var multiplicand = parseInt(req.params.multiplicand);
-  var multiplier = parseInt(req.params.multiplier);
-  if (isNaN(multiplicand) || isNaN(multiplier)) {
-    res.status(400).json({ error: 'Invalid parameters' });
-    return;
-  }
-
-  // Multiply the values and send the result as a JSON response
-  var result = multiplicand * multiplier;
-  res.status(200).json({ result : result });
+router.use(jsend.middleware);
+router.get('/:number1/:number2', function(req, res, next) {
+    const number1 = parseInt(req.params.number1);
+    if(isNaN(number1)) {
+        return res.jsend.fail({"number1": "number1 is not in correct format"});
+    }
+    const number2 = parseInt(req.params.number2);
+    if(isNaN(number2)) {
+        return res.jsend.fail({"number2": "number2 is not in correct format"});
+    }
+    const result = number1 * number2;
+    res.jsend.success(result);
 });
 
 module.exports = router;
